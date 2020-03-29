ae376b778bfa1c201fdbaff1da23857f580ac1cb
dev-augustin <dev.augustin00@gmail.com>
2020-03-29 00:30:26 -0500

    added pseudocode

---
+++ stopwatch/pseudocode/pseudocode.js
@@ -0,0 +1,6 @@
+// Stop watch with three buttons: start, pause and reset
+// On Start, time increment should be displayed
+// On Reset, stopwatch stops and value is 0;
+// On Pause, stopwatch should pause at currenttime
+// On reclicking Pause, stopwatch should resume time
+// Use setInterval, clearInterval
