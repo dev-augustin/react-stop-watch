b1f92fe620fb70c9b719762bf0fabfd542c129ee
dev-augustin <dev.augustin00@gmail.com>
2020-03-29 01:07:07 -0500

    added setInterval function

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import './App.css';
-
+import Header from './components/Header'
 
 class App extends React.Component {
   constructor(props){
     super(props);
-    
+    this.state=
+    {startTime:2}
   }
-  
+  startTimer(){
+    
+    //console.log("timerStarts")
+    setInterval(this.state.startTime, 1000)
+   
+
+  }
+ 
   render()
   {
     return (
     <div> 
-      <h2>Stop Watch</h2>
+      <Header/>
+      <div className="counter">{this.state.startTime}</div>
       <div className='centerButton'>
-        <button id="start">Start</button>
+        <button id="start" onClick={()=>this.startTimer()}>Start</button>
         <button id="pause">Pause</button>
         <button id="stop">Stop</button>
 
