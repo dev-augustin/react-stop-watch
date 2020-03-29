180ec990a46fb873b547c0f77c0565a2a4e159d8
dev-augustin <dev.augustin00@gmail.com>
2020-03-29 07:49:55 -0500

    added pause functionality

--- stopwatch/src/components/Timer.js
+++ stopwatch/src/components/Timer.js
@@ -4,21 +4,36 @@ class Timer extends React.Component {
     constructor(props){
       super(props);
       this.state=
-      {startTime:0}
+      {startTime:0,
+       isTimerState:true,
+       intervalId:0
     }
-  
-   
+    }
+     startTime(){
+         this.intervalId=(
+                setInterval(()=>{this.setState({
+                    startTime: this.state.startTime+1,
+                    isTimerState: false})},
+                    1000))
+                   
+         }     
+         
+         pauseTime(){
+            clearInterval(this.intervalId)
+                      
+            }   
+     
     render()
     {
       return (
   
       <div> 
       
-        <div className="counter"></div>
+        <div className="counter">Time: {this.state.startTime}</div>
         <div className='centerButton'>
-          <button id="start" onClick={()=>this.startTimer()}>Start</button>
-          <button id="pause">Pause</button>
-          <button id="stop">Stop</button>
+          <button id="start" onClick={()=>this.startTime()}>Start</button>
+          <button id="pause" onClick={()=>this.pauseTime()}>Pause</button>
+          <button id="stop" onClick={()=>this.stopTime()}>Reset</button>
   
         </div>  
       
