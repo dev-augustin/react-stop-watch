19fe35041188439a29b7007d5d1bc4cb2f7d61a0
dev-augustin <dev.augustin00@gmail.com>
2020-03-29 01:21:39 -0500

    moved files to component folder

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,38 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
-
+import Timer from './components/Timer'
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state=
-    {startTime:2}
-  }
-  startTimer(){
-    
-    //console.log("timerStarts")
-    setInterval(this.state.startTime, 1000)
-   
 
-  }
- 
   render()
   {
     return (
+
     <div> 
       <Header/>
-      <div className="counter">{this.state.startTime}</div>
-      <div className='centerButton'>
-        <button id="start" onClick={()=>this.startTimer()}>Start</button>
-        <button id="pause">Pause</button>
-        <button id="stop">Stop</button>
+      <Timer/>
 
       </div>  
-    
-    </div>
-  );
-}}
-
+     );
+}
+}
 
 export default App;
 
